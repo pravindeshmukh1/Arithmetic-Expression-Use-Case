b84b02030d687ac49ac8749b7c2ca4f0db4dc8e3
pravindeshmukh1 <pravin1desh@gmail.com>
2020-03-06 12:35:02 +0530

    Display Result in Decending order

--- arithmeticExpression.sh
+++ arithmeticExpression.sh
@@ -4,9 +4,9 @@ echo "Sorting Arithmetic Computation Problem UC_7 "
 
 read -p "Enter the Three Input: " a b c
 
-x=$(echo "$a+$b*$c" | bc )
-y=$(echo "$a*$b+$c" | bc )
-z=$(echo "$c+$a/$b" | bc )
+x=$(( $a+$b*$c ))
+y=$(( $a*$b+$c ))
+z=$(echo "scale=2; $c+$a/$b" | bc )
 p=$(echo "$a%$b+$c" | bc )
 
 a[1]=$x
@@ -15,8 +15,14 @@ a[3]=$z
 a[4]=$p
 
 echo "Dict " ${a[@]}
-#read the Dict and store in Array
+#read the Dict and store in a
+count=o
 for i in ${a[@]}
 do
-echo "value" ${a[@]}
+	a[((count++))]=$i
 done
+echo "array" ${a[@]}
+
+# sort the result decending order
+printf '%s\n' "${a[@]}" | sort -n -r
+
