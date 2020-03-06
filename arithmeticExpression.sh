f1517f631db4695c9ab43ddab6ae05b6bea5dcde
pravindeshmukh1 <pravin1desh@gmail.com>
2020-03-06 11:20:19 +0530

    ADDED : Read the Dictionary and Store in Array

--- arithmeticExpression.sh
+++ arithmeticExpression.sh
@@ -1,12 +1,22 @@
 #!/bin/bash -x
 
-echo "Sorting Arithmetic Computation Problem UC_6 "
+echo "Sorting Arithmetic Computation Problem UC_7 "
 
 read -p "Enter the Three Input: " a b c
 
-z=$(($a + $b * $c))
-y=$(($a * $b + $c))
-x=$(($c + $a / $b))
-p=$(($a % $b + $c))
-dict=([1]=$z [2]=$y [3]=$x [4]=$p)
-echo ${dict[@]}
+x=$(echo "$a+$b*$c" | bc )
+y=$(echo "$a*$b+$c" | bc )
+z=$(echo "$c+$a/$b" | bc )
+p=$(echo "$a%$b+$c" | bc )
+
+a[1]=$x
+a[2]=$y
+a[3]=$z
+a[4]=$p
+
+echo "Dict " ${a[@]}
+#read the Dict and store in Array
+for i in ${a[@]}
+do
+echo "value" ${a[@]}
+done
