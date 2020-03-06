1508def5a45b9fd9258084d75a281f6467558df0
pravindeshmukh1 <pravin1desh@gmail.com>
2020-03-06 09:27:36 +0530

    ADDED : Compute user input

--- arithmeticExpression.sh
+++ arithmeticExpression.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -x
 
-echo "Sorting Arithmetic Computation Problem UC_1 "
+echo "Sorting Arithmetic Computation Problem UC_2 "
 
 read -p "Enter the Three Input: " a b c
-echo $a $b $c
 
+z=$(($a + $b * $c))
+echo $z
